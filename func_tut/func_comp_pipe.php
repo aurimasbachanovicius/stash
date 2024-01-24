0af72a121e33175632a92c35b9451bacdd5e1178
Aurimas Bachanovicius <aurimas.bachanovicius@metasite.net>
2024-01-24 15:39:54 +0200

    pipe

--- func_tut/func_comp_pipe.php
+++ func_tut/func_comp_pipe.php
@@ -1,16 +1,11 @@
 <?php
-$pipe = fn($value, ...$functions) => array_reduce($functions, fn($carry, $function) => $function($carry), $value);
+$pipe = fn(mixed $value, callable ...$functions) => array_reduce($functions, fn(mixed $carry, callable $function) => $function($carry), $value);
 
-$filterActiveUsers = fn($data) => $data . ' - Active Users Filtered';
-$sortData = fn($data) => $data . ' - Data Sorted';
-$filterBad = fn($data) => $data . ' - Bad Data Filtered';
-$prepareReport = fn($data) => $data . ' - Report Prepared';
+$filterActiveUsers = fn(string $data): string => $data . ' - Active Users Filtered';
+$sortData = fn(string $data): string => $data . ' - Data Sorted';
+$filterBad = fn(string $data): string => $data . ' - Bad Data Filtered';
+$prepareReport = fn(string $data): string => $data . ' - Report Prepared';
 
-$result = $pipe('testingData',
-    $filterActiveUsers,
-    $sortData,
-    $filterBad,
-    $prepareReport,
-);
-
-echo $result;
+echo $pipe('testingData', $filterActiveUsers, $sortData, $filterBad, $prepareReport);
+echo "\n";
+echo $pipe('testingData', $filterActiveUsers, $prepareReport, $sortData, $filterBad);
